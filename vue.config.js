d95d52fe290b5d57385ba650e5fb77ee542618e0
Jayden(안홍범) <anbum8463@gmail.com>
2023-12-08 15:09:44 +0900

    환경셋팅

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  outputDir: 'dist',
+  /* webpack config 옵션 추가 */
+  configureWebpack: {
+    plugins: []
+  },
+
+  /* 옵션 체이닝 */
+  chainWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      // mutate config for production...
+    } else {
+      // mutate for development...
+    }
+  },
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "@/assets/scss/reset.scss";
+        `
+      }
+    }
+  }
+})
