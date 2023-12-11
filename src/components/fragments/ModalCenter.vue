d0b7067da82827e8cd80812fba28315ad9ddafbf
Jayden(안홍범) <anbum8463@gmail.com>
2023-12-11 15:45:10 +0900

    moved code for common

--- src/components/fragments/ModalCenter.vue
+++ src/components/fragments/ModalCenter.vue
@@ -25,46 +25,4 @@ export default {
 }
 </script>
 
-<style lang="scss">
-.popup {
-  display: none;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  padding: rem(40px);
-  background-color: rgba($color: $neutral-dark, $alpha: 0.3);
-  &.active {
-    display: flex;
-  }
-  &--area {
-    position: relative;
-    padding: rem(32px 24px);
-    background-color: $neutral-white;
-    border-radius: rem(16px);
-  }
-  &--header {
-    h4 {
-      font-size: rem(20px);
-      text-align: center;
-      font-weight: 700;
-    }
-  }
-  &--body {
-    margin-top: rem(8px);
-    &__contnet {
-      font-size: rem(16px);
-      color: $neutral-darkgrey;
-      text-align: center;
-    }
-  }
-  &--footer {
-    margin-top: rem(48px);
-    text-align: center;
-  }
-}
-</style>
+<style lang="scss"></style>
