d7bd512be3498df1d8ffa59cdd626a8652ea41a8
DaHyun <dhnam1128@gmail.com>
2023-12-11 15:57:49 +0900

    버튼 컴포넌트 작업 중
    버튼 컴포넌트 작업 중

---
+++ src/components/fragments/ButtonCmp.vue
@@ -0,0 +1,69 @@
+<template>
+  <button :class="btnOptions" class="btn">
+    <div>
+      <span class="btn-txt bg-btn__txt">{{ btnBgTxt }}</span>
+      <i class="btn-link" v-if="btnLink">
+        <img
+          src="@/assets/images/icon/arrow-right.png"
+          :class="[
+            btnLinkRight ? 'btn-link__right' : '',
+            !btnLinkRight && center ? 'center' : ''
+          ]"
+        />
+        <span class="hidden">{{ btnBgTxt }} 바로가기</span>
+      </i>
+    </div>
+  </button>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  props: {
+    btnBg: {
+      type: Boolean,
+      default: false
+    },
+    btnBd: {
+      type: Boolean,
+      default: false
+    },
+    btnBgTxt: {
+      type: String,
+      default: 'Block + Primary'
+    },
+    btnLink: {
+      type: Boolean,
+      default: false
+    },
+    linkDefault: {
+      type: Boolean,
+      default: false
+    },
+    btnLinkRight: {
+      type: Boolean,
+      default: false
+    },
+    center: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    btnOptions() {
+      return {
+        'bg-btn': this.btnBg,
+        'bd-btn': this.btnBd,
+        'hover-btn': this.btnBg && this.btnBgHover,
+        [this.type]: true,
+        [this.size]: true
+      }
+    },
+    btnBgHover() {
+      return true
+    }
+  }
+}
+</script>
+
+<style lang="scss"></style>
