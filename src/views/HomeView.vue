bdb0a864ceb527aa5fa097116eaf27182892cc6a
DaHyun <dhnam1128@gmail.com>
2023-12-11 16:15:48 +0900

    버튼 컴포넌트 > app.vue에 적용
    버튼 컴포넌트 > app.vue에 적용

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,50 @@
 <template>
   <div>
     <title-input />
+    <div class="guide__wrap">
+      <!-- 1. 기본 : 배경색 -->
+      <div class="guide-box">
+        <ButtonCmp btnBg type="primary-base" size="large" />
+        <ButtonCmp btnBg type="primary-dark" size="large" />
+        <ButtonCmp btnBg type="neutral-basegrey" size="large" />
+      </div>
+      <!-- 1-1. 기본 : 배경색 + 왼쪽 아이콘-->
+      <div class="guide-box">
+        <ButtonCmp
+          btnBg
+          type="primary-base"
+          size="large"
+          btnLink
+          btnLinkRight
+        />
+        <ButtonCmp
+          btnBg
+          type="primary-dark"
+          size="large"
+          btnLink
+          btnLinkRight
+        />
+        <ButtonCmp
+          btnBg
+          type="neutral-basegrey"
+          size="large"
+          btnLink
+          btnLinkRight
+        />
+      </div>
+      <!-- 1-2. 기본 : 배경색 + 오른쪽 아이콘-->
+      <div class="guide-box">
+        <ButtonCmp btnBg type="primary-base" size="large" btnLink />
+        <ButtonCmp btnBg type="primary-dark" size="large" btnLink />
+        <ButtonCmp btnBg type="neutral-basegrey" size="large" btnLink />
+      </div>
+      <!-- 1-3. 기본 : 배경색 + 중간 아이콘-->
+      <div class="guide-box">
+        <ButtonCmp btnBg type="primary-base" size="large" btnLink center />
+        <ButtonCmp btnBg type="primary-dark" size="large" btnLink center />
+        <ButtonCmp btnBg type="neutral-basegrey" size="large" btnLink center />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,9 +52,10 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import TitleInput from '@/components/fragments/TitleInput.vue'
+import ButtonCmp from '@/components/fragments/ButtonCmp.vue'
 
 export default {
-  components: { TitleInput }
+  components: { TitleInput, ButtonCmp }
 }
 </script>
 <style lang="scss"></style>
