5eb7c7c51b6456fc8cce4ec560dc5ee0afb83467
100minkyung <mkbaek@the-51.com>
2023-12-12 18:03:09 +0900

    인풋 컴포넌트 작업중

--- src/components/fragments/inputField.vue
+++ src/components/fragments/inputField.vue
@@ -1,41 +1,41 @@
 <template>
-  <label
-    :for="id"
-    :class="disabled === false ? 'inputField' : 'inputField--disabled'"
-  >
-    <p v-if="label" class="inputField__label">{{ label }}</p>
-    <select
-      v-if="type === 'select'"
-      :class="
-        warn === false
-          ? 'inputField__input'
-          : 'inputField__input inputField__input--warn'
-      "
-      :id="id"
-      :name="name"
-      :readonly="disabled"
-    >
+  <div :class="readonly === false ? 'inputField' : 'inputField inputField--disabled'">
+    <!-- 타이틀 -->
+    <p v-if="title" class="inputField__title">{{ title }}</p>
+    <label :for="id" :class="type === 'file' ? 'inputField__file--label' : ''"></label>
+    <!-- 셀렉트 -->
+    <select v-if="type === 'select'" :class=" warn === false ? 'inputField__input' : 'inputField__input inputField__input--warn'" :id="id" :name="name" :readonly="readonly">
       <option disabled selected hidden>{{ placeholder }}</option>
-      <option :key="i" :value="options.option" v-for="(options, i) in options">
-        {{ options.option }}
-      </option>
+      <option :key="i" v-for="(options, i) in options" :value="options">{{ options }}</option>
     </select>
-    <input
-      v-else
-      :type="type"
-      :class="
-        warn === false
-          ? 'inputField__input'
-          : 'inputField__input inputField__input--warn'
-      "
+    <!-- 텍스트에리어 -->
+    <textarea v-else-if="type === 'textarea'" :type="type" class="inputField__text" :id="id" :name="name" :placeholder="placeholder" @input="textareaHegiht" rows="1"></textarea>
+    <!-- 체크박스 -->
+    <input v-else-if="type === 'checkbox'" :type="type" :class="warn === false ? 'inputField__input' : 'inputField__input inputField__input--warn'"
       :id="id"
       :name="name"
       :placeholder="placeholder"
-      :readonly="disabled"
-    />
-    <button v-if="type === 'password'" class="inputField__button" @click="passworView" :disabled=disabled></button>
+      :readonly="readonly"
+      :checked="checked"
+      @change="checkedText">
+    <!-- 파일 -->
+    <input v-else-if="type === 'file'" :type="type" class="inputField__file"
+      :id="id"
+      :name="name"/>
+    <!-- 그외 -->
+    <input v-else :type="type" :class="warn === false ? 'inputField__input' : 'inputField__input inputField__input--warn'"
+      :id="id"
+      :name="name"
+      :placeholder="placeholder"
+      :readonly="readonly"
+      :checked="checked"/>
+    <!-- 패스워드 눈 버튼 -->
+    <button v-if="type === 'password'" class="inputField__button" @click="passwordView(event)" :readonly=readonly></button>
+    <!-- 체크박스 텍스트에리어 -->
+    <textarea v-if="type === 'checkbox'" :class="checked ? 'inputField__text inputField__text--side checked' : 'inputField__text inputField__text--side'" :id =" id + '__text'" :name="name" :placeholder="placeholder" @input="textareaHegiht" rows="1"></textarea>
+    <!-- 캡션 -->
     <p v-if="caption" class="inputField__caption">{{ caption }}</p>
-  </label>
+  </div>
 </template>
 
 <script>
@@ -45,48 +45,59 @@ export default {
       type: String,
       default: 'text'
     },
-    label: {
-      type: String
-    },
-    caption: {
-      type: String
-    },
-    id: {
-      type: String,
-      default: 'id'
-    },
-    name: {
-      type: String,
-      default: 'name'
-    },
-    placeholder: {
-      type: String,
-      default: 'Placeholder'
-    },
-    disabled: {
+    title: String,
+    caption: String,
+    id: String,
+    name: String,
+    placeholder: String,
+    readonly: {
       type: Boolean,
       default: false
     },
     warn: {
       type: Boolean,
       default: false
-    }
-  },
-  data () {
-    return {
-      options: [{ option: 'r' }, { option: 'i' }, { option: 'd' }]
+    },
+    checked: {
+      type: Boolean,
+      default: false
+    },
+    options: {
+      type: Array,
+      default: () => {
+        return ['r', 'd']
+      }
     }
   },
   methods: {
-    passworView: function (el) {
-      const target = el.currentTarget.previousSibling
-      target.classList.toggle('on')
-      if (target.classList.contains('on')) {
-        target.setAttribute('type', 'text')
-        el.currentTarget.style.filter = 'opacity(25%)'
+    // 패스워드 눈 버튼
+    passwordView: ($event) => {
+      console.log(this, $event)
+      // const target = el.currentTarget.previousElementSibling
+      // if (this.readonly === false) {
+      //   target.classList.toggle('on')
+      //   if (target.classList.contains('on')) {
+      //     target.setAttribute('type', 'text')
+      //     el.currentTarget.style.filter = 'opacity(25%)'
+      //   } else {
+      //     target.setAttribute('type', 'password')
+      //     el.currentTarget.style.filter = 'opacity(100%)'
+      //   }
+      // }
+    },
+    // 텍스트에리어 높이 자동조절
+    textareaHegiht: function (el) {
+      const target = el.currentTarget
+      target.style.height = 'auto'
+      target.style.height = target.scrollHeight + 'px'
+    },
+    // 체크박스 체크하면 텍스트 줄치기
+    checkedText: function (el) {
+      const target = el.currentTarget
+      if (target.checked === true && target.nextElementSibling.value.length !== 0) {
+        target.nextElementSibling.classList.add('checked')
       } else {
-        target.setAttribute('type', 'password')
-        el.currentTarget.style.filter = 'opacity(100%)'
+        target.nextElementSibling.classList.remove('checked')
       }
     }
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,33 @@
 <template>
   <div>
-    <TitleInput />
+    <inputField type="text" title="input_default" caption="input_default" id="input_default" name="input_default" placeholder="input_default"></inputField>
+    <inputField type="text" title="input_warn" id="input_warn" name="input_warn" placeholder="input_warn" warn></inputField>
+    <inputField type="text" title="input_readonly" id="input_readonly" name="input_readonly" placeholder="input_readonly" readonly></inputField>
+    <inputField type="password" id="password_default" name="password_default" placeholder="********"></inputField>
+    <inputField type="password" id="password_warn" name="password_warn" placeholder="********" warn></inputField>
+    <inputField type="password" id="password_readonly" name="password_readonly" placeholder="********" readonly></inputField>
+    <inputField type="select" title="select_default" caption="select_default" id="select_default" name="select_default" placeholder="select_default" :options="options"></inputField>
+    <inputField type="select" title="select_warn" id="select_warn" name="select_warn" placeholder="select_warn" warn></inputField>
+    <inputField type="select" title="select_readonly" id="select_readonly" name="select_readonly" placeholder="select_readonly" readonly></inputField>
+    <inputField type="checkbox" title="checkbox_default" id="checkbox_default" placeholder="Write your notes here..."></inputField>
+    <inputField type="checkbox" title="checkbox_checked" id="checkbox_checked" placeholder="Write your notes here..." checked></inputField>
+    <inputField type="textarea" title="textarea_default" id="text_default" placeholder="Write your notes here..."></inputField>
+    <inputField type="file" title="file_default" id="file_default"></inputField>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-import TitleInput from '@/components/fragments/TitleInput.vue'
+import inputField from '@/components/fragments/inputField.vue'
 
 export default {
-  components: { TitleInput }
+  components: { inputField },
+  data () {
+    return {
+      options: ['t', 'e', 's', 't']
+    }
+  }
 }
 </script>
 <style lang="scss"></style>
