fa0dfb22877d2a9227a3a5ab1d5e4e6c75bdcf29
Jayden(안홍범) <anbum8463@gmail.com>
2023-12-12 11:04:10 +0900

    modal bottom

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ModalCenter from '@/components/fragments/ModalCenter.vue'
+import ModalBottom from '@/components/fragments/ModalBottom.vue'
 import common from './assets/js/common'
 
 const app = createApp(App)
@@ -10,4 +11,5 @@ app.use(store)
 app.use(router)
 app.mixin(common)
 app.component('ModalCenter', ModalCenter)
+app.component('ModalBottom', ModalBottom)
 app.mount('#app')
--- src/views/OverlaysView.vue
+++ src/views/OverlaysView.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <button @click="openBtn('success-modal')">open</button>
-    <modal-center id="success-modal">
+    <button @click="openBtn('modal')">modal</button>
+    <br />
+    <button @click="openBtn('success-modal')">success-modal</button>
+    <br />
+    <button @click="openBtn('modal-bottom')">modal-bottom</button>
+    <br />
+    <button @click="openBtn('modal-bottom2')">modal-bottom2</button>
+    <modal-center id="modal">
       <template v-slot:title>
         <h4>Modal Title</h4>
       </template>
@@ -14,11 +20,96 @@
         <button>Action</button><button @click="closeBtn($event)">Close</button>
       </template>
     </modal-center>
+
+    <modal-center id="success-modal">
+      <template v-slot:title>
+        <h4 class="success">Modal Title</h4>
+      </template>
+      <template v-slot:body>
+        <div class="popup--body__contnet">
+          Write your detailed description for this modal here
+        </div>
+      </template>
+      <template v-slot:footer>
+        <button>Action</button><button @click="closeBtn($event)">Close</button>
+      </template>
+    </modal-center>
+
+    <modal-bottom id="modal-bottom">
+      <template v-slot:title> </template>
+      <template v-slot:body>
+        <button class="eestore">Restore This Note</button>
+        <button class="deleteBtn">Delete Note</button>
+      </template>
+      <template v-slot:footer>
+        <button @click="closeBtn($event)">close</button>
+      </template>
+    </modal-bottom>
+
+    <modal-bottom id="modal-bottom2">
+      <template v-slot:title> </template>
+      <template v-slot:body>
+        <div class="extras">
+          <p class="extras__title">EXTRAS</p>
+          <ul>
+            <li :key="list" v-for="list in lists">
+              <a href="" :data-icon="list.type">{{ list.text }}</a>
+            </li>
+          </ul>
+        </div>
+        <button class="deleteBtn">Delete Note</button>
+      </template>
+      <template v-slot:footer>
+        <button @click="closeBtn($event)">close</button>
+      </template>
+    </modal-bottom>
   </div>
 </template>
 
 <script>
 /* eslint-disable */
-export default {}
+export default {
+  data() {
+    return {
+      lists: [
+        { type: '1', text: 'Set Reminder', flag: 'Not set' },
+        { type: '2', text: 'Change Note Type', flag: 'Buying Some...' },
+        { type: '3', text: 'Give Label', flag: 'Not set' },
+        { type: '4', text: 'Mark as Finished', flag: '' }
+      ]
+    }
+  }
+}
 </script>
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.restore {
+  width: 100%;
+  line-height: rem(56px);
+  color: $neutral-dark;
+  font-size: rem(16px);
+  font-weight: 500;
+}
+.deleteBtn {
+  width: 100%;
+  line-height: rem(56px);
+  color: $error-base;
+  font-size: rem(16px);
+  font-weight: 500;
+}
+.extras {
+  &__title {
+    font-size: rem(10px);
+    line-height: rem(12px);
+    color: $neutral-darkgrey;
+  }
+  ul {
+    margin-top: rem(8px);
+    li {
+      a {
+        display: block;
+        line-height: rem(56px);
+      }
+    }
+  }
+}
+</style>
