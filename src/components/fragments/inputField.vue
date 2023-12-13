5fcd54ed22609ac66ed97faf17af0da26b5e0144
100minkyung <mkbaek@the-51.com>
2023-12-13 17:01:24 +0900

    인풋 작업중

--- src/components/fragments/inputField.vue
+++ src/components/fragments/inputField.vue
@@ -2,26 +2,33 @@
   <div :class="readonly === false ? 'inputField' : 'inputField inputField--disabled'">
     <!-- 타이틀 -->
     <p v-if="title" class="inputField__title">{{ title }}</p>
-    <label :for="id" :class="type === 'file' ? 'inputField__file--label' : ''"></label>
     <!-- 셀렉트 -->
-    <select v-if="type === 'select'" :class=" warn === false ? 'inputField__input' : 'inputField__input inputField__input--warn'" :id="id" :name="name" :readonly="readonly">
+    <select v-if=" type === 'select'" :class=" warn === false ? 'inputField__input' : 'inputField__input inputField__input--warn'" :id="id" :name="name" :readonly="readonly">
       <option disabled selected hidden>{{ placeholder }}</option>
       <option :key="i" v-for="(options, i) in options" :value="options">{{ options }}</option>
     </select>
     <!-- 텍스트에리어 -->
-    <textarea v-else-if="type === 'textarea'" :type="type" class="inputField__text" :id="id" :name="name" :placeholder="placeholder" @input="textareaHegiht" rows="1"></textarea>
+    <textarea v-else-if=" type === 'textarea'" :type="type" class="inputField__text" :id="id" :name="name" :placeholder="placeholder" @input="textareaHegiht" rows="1"></textarea>
     <!-- 체크박스 -->
-    <input v-else-if="type === 'checkbox'" :type="type" :class="warn === false ? 'inputField__input' : 'inputField__input inputField__input--warn'"
+    <input v-else-if=" type === 'checkbox'" :type="type"
+      :class="[
+        size === 'small' ? 'inputField__input inputField__input__checkbox inputField__input__checkbox--small' : 'inputField__input inputField__input__checkbox',
+        round === 'round' ? 'inputField__input__checkbox--round' : '',
+        color === 'highlight' ? 'inputField__input__checkbox--highlight' : '']"
       :id="id"
       :name="name"
-      :placeholder="placeholder"
       :readonly="readonly"
       :checked="checked"
       @change="checkedText">
-    <!-- 파일 -->
-    <input v-else-if="type === 'file'" :type="type" class="inputField__file"
+    <!-- 토글 -->
+    <input v-else-if=" type === 'toggle'" type="checkbox"
+      class="inputField__toggle"
       :id="id"
-      :name="name"/>
+      :name="name"
+      :readonly="readonly"
+      :checked="checked">
+    <!-- 파일 -->
+    <input v-else-if=" type === 'file'" :type="type" class="inputField__file" :id="id" :name="name"/>
     <!-- 그외 -->
     <input v-else :type="type" :class="warn === false ? 'inputField__input' : 'inputField__input inputField__input--warn'"
       :id="id"
@@ -30,9 +37,24 @@
       :readonly="readonly"
       :checked="checked"/>
     <!-- 패스워드 눈 버튼 -->
-    <button v-if="type === 'password'" class="inputField__button" @click="passwordView(event)" :readonly=readonly></button>
+    <button v-if="type === 'password'" class="inputField__button" :readonly=readonly @click="passwordView"></button>
     <!-- 체크박스 텍스트에리어 -->
-    <textarea v-if="type === 'checkbox'" :class="checked ? 'inputField__text inputField__text--side checked' : 'inputField__text inputField__text--side'" :id =" id + '__text'" :name="name" :placeholder="placeholder" @input="textareaHegiht" rows="1"></textarea>
+    <textarea v-if="type === 'checkbox'" :class="checked ? 'inputField__text inputField__text--side checked' : 'inputField__text inputField__text--side'"
+    :id =" id + '__text'"
+    :name="name"
+    :placeholder="placeholder"
+    :value="defaultText"
+    @input="textareaHegiht"
+    rows="1"
+    :readonly="checked"></textarea>
+    <!-- 라벨 -->
+    <label :for="id"
+    :class="[
+      type === 'file' ? 'inputField__file--label' : '',
+      type === 'toggle' ? 'inputField__toggle--label' : '',
+      type === 'toggle' && color === 'highlight' ? 'inputField__toggle--label inputField__toggle--label--highlight' : '']">
+      <span v-if="type === 'toggle'" class="inputField__toggle--label--circle"></span>
+    </label>
     <!-- 캡션 -->
     <p v-if="caption" class="inputField__caption">{{ caption }}</p>
   </div>
@@ -50,6 +72,10 @@ export default {
     id: String,
     name: String,
     placeholder: String,
+    defaultText: String,
+    size: String,
+    round: String,
+    color: String,
     readonly: {
       type: Boolean,
       default: false
@@ -71,19 +97,18 @@ export default {
   },
   methods: {
     // 패스워드 눈 버튼
-    passwordView: ($event) => {
-      console.log(this, $event)
-      // const target = el.currentTarget.previousElementSibling
-      // if (this.readonly === false) {
-      //   target.classList.toggle('on')
-      //   if (target.classList.contains('on')) {
-      //     target.setAttribute('type', 'text')
-      //     el.currentTarget.style.filter = 'opacity(25%)'
-      //   } else {
-      //     target.setAttribute('type', 'password')
-      //     el.currentTarget.style.filter = 'opacity(100%)'
-      //   }
-      // }
+    passwordView: (el) => {
+      const target = el.currentTarget.previousElementSibling
+      if (target.readOnly !== true) {
+        target.classList.toggle('on')
+        if (target.classList.contains('on')) {
+          target.setAttribute('type', 'text')
+          el.currentTarget.style.filter = 'opacity(25%)'
+        } else {
+          target.setAttribute('type', 'password')
+          el.currentTarget.style.filter = 'opacity(100%)'
+        }
+      }
     },
     // 텍스트에리어 높이 자동조절
     textareaHegiht: function (el) {
@@ -94,10 +119,24 @@ export default {
     // 체크박스 체크하면 텍스트 줄치기
     checkedText: function (el) {
       const target = el.currentTarget
-      if (target.checked === true && target.nextElementSibling.value.length !== 0) {
-        target.nextElementSibling.classList.add('checked')
+      const input = target.nextElementSibling
+      if (input.value.length === 0) {
+        target.checked = false
+      }
+      if (target.checked === true && input.value.length !== 0) {
+        input.classList.add('checked')
       } else {
-        target.nextElementSibling.classList.remove('checked')
+        input.classList.remove('checked')
+      }
+      if (target.checked === true) {
+        input.readOnly = true
+      } else {
+        input.readOnly = false
+      }
+      if (target.readOnly === true) {
+        target.checked = true
+        input.readOnly = true
+        input.classList.add('checked')
       }
     }
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,9 +9,23 @@
     <inputField type="select" title="select_default" caption="select_default" id="select_default" name="select_default" placeholder="select_default" :options="options"></inputField>
     <inputField type="select" title="select_warn" id="select_warn" name="select_warn" placeholder="select_warn" warn></inputField>
     <inputField type="select" title="select_readonly" id="select_readonly" name="select_readonly" placeholder="select_readonly" readonly></inputField>
-    <inputField type="checkbox" title="checkbox_default" id="checkbox_default" placeholder="Write your notes here..."></inputField>
-    <inputField type="checkbox" title="checkbox_checked" id="checkbox_checked" placeholder="Write your notes here..." checked></inputField>
     <inputField type="textarea" title="textarea_default" id="text_default" placeholder="Write your notes here..."></inputField>
+    <inputField type="checkbox" title="checkbox_default" id="checkbox_default" placeholder="Write your notes here..."></inputField>
+    <inputField type="checkbox" title="checkbox_checked" id="checkbox_checked" placeholder="Write your notes here..." defaultText="Checklist" checked></inputField>
+    <inputField type="checkbox" title="checkbox_highlight" id="checkbox_hilight" color="highlight" placeholder="Write your notes here..." defaultText="Checklist" checked></inputField>
+    <inputField type="checkbox" title="checkbox_readonly" id="checkbox_readonly" placeholder="Write your notes here..." defaultText="Checklist" checked readonly></inputField>
+    <inputField type="checkbox" title="checkbox_small_checked" id="checkbox_small_checked" size="small" placeholder="Write your notes here..." defaultText="Checklist" checked></inputField>
+    <inputField type="checkbox" title="checkbox_small_highlight" id="checkbox_small_highlight" size="small" color="highlight" placeholder="Write your notes here..." defaultText="Checklist" checked></inputField>
+    <inputField type="checkbox" title="checkbox_small_readonly" id="checkbox_small_readonly" size="small" placeholder="Write your notes here..." defaultText="Checklist" checked readonly></inputField>
+    <inputField type="checkbox" title="checkbox_round_checked" id="checkbox_round_checked" round="round" placeholder="Write your notes here..." defaultText="Checklist" checked></inputField>
+    <inputField type="checkbox" title="checkbox_round_highlight" id="checkbox_round_highlight" round="round" color="highlight" placeholder="Write your notes here..." defaultText="Checklist" checked></inputField>
+    <inputField type="checkbox" title="checkbox_round_readonly" id="checkbox_round_readonly" round="round" placeholder="Write your notes here..." defaultText="Checklist" checked readonly></inputField>
+    <inputField type="checkbox" title="checkbox_small_round_checked" id="checkbox_small_round_checked" size="small" round="round" placeholder="Write your notes here..." defaultText="Checklist" checked></inputField>
+    <inputField type="checkbox" title="checkbox_small_round_highlight" id="checkbox_small_round_highlight" size="small" round="round" color="highlight" placeholder="Write your notes here..." defaultText="Checklist" checked></inputField>
+    <inputField type="checkbox" title="checkbox_small_round_readonly" id="checkbox_small_round_readonly" size="small" round="round" placeholder="Write your notes here..." defaultText="Checklist" checked readonly></inputField>
+    <inputField type="toggle" title="toggle_default" id="toggle_default"></inputField>
+    <inputField type="toggle" title="toggle_highlight" color="highlight" id="toggle_highlight"></inputField>
+    <inputField type="toggle" title="toggle_readonly" id="toggle_readonly" readonly></inputField>
     <inputField type="file" title="file_default" id="file_default"></inputField>
   </div>
 </template>
