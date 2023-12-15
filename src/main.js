710f34751e19d67f323a8e48b6e5c3c2baa176ce
DaHyun <dhnam1128@gmail.com>
2023-12-15 10:17:10 +0900

    충돌부분 삭제
    충돌부분 삭제

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ import ButtonCmp from '@/components/fragments/ButtonCmp.vue'
 import listCmp from '@/components/fragments/listCmp.vue'
 import inputField from '@/components/fragments/inputField.vue'
 import tagCmp from '@/components/fragments/TagCmp.vue'
-import inputField from '@/components/fragments/inputField.vue'
 import common from './assets/js/common'
 
 const app = createApp(App)
@@ -23,5 +22,4 @@ app.component('ButtonCmp', ButtonCmp)
 app.component('listCmp', listCmp)
 app.component('inputField', inputField)
 app.component('tagCmp', tagCmp)
-app.component('inputField', inputField)
 app.mount('#app')
