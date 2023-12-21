4e68685127ddf1dcbe45ca68e6f581557eab18bd
Jayden(안홍범) <anbum8463@gmail.com>
2023-12-21 11:20:41 +0900

    Update searchView.vue

--- src/views/search/searchView.vue
+++ src/views/search/searchView.vue
@@ -7,14 +7,15 @@
         id="search"
         placeholder="Search..."
         v-model:searchValue="result"
-        @keyup.enter="addRecent(result)"
+        @keyup.enter="addRecent($event)"
       ></inputField>
     </div>
     <div class="search__bottom">
       <p>RECENT SEARCHES</p>
       <ul>
-        <li><router-link :to="{ name: 'defaultHome' }">Product Idea</router-link></li>
-        <li><router-link :to="{ name: 'defaultHome' }">Monthly Buying List</router-link></li>
+        <li :key="index" v-for="(list, index) in seachList.slice().reverse()">
+          <router-link :to="{ name: list.route }">{{ list.item }}</router-link>
+        </li>
       </ul>
     </div>
   </div>
@@ -31,81 +32,79 @@ const goUrl = (url) => {
 }
 
 const result = ref('')
+const seachList = ref([
+  { item: 'Product Idea', route: 'defaultHome' },
+  { item: 'Monthly Buying List', route: 'defaultHome' }
+])
 
-const addRecent = (result) => {
-  const target = document.querySelector('.search__bottom ul')
-  const el = `<li><a herf="/home/default">${result}</a></li>`
-  target.insertAdjacentHTML('afterbegin', el)
-  console.log(target.lastChild)
-  if (target.childElementCount > 5) {
-    target.removeChild(target.lastChild)
-  }
-  // const li = document.createElement('li')
-  // const link = document.createElement('a')
-  // const resultValue = document.createTextNode(result)
-  // link.setAttribute('href', '/home/default')
-  // link.appendChild(resultValue)
-  // li.appendChild(link)
-  // target.appendChild(li)
+const addRecent = (event) => {
+  if (result.value === '') return false
+  const searchResult = { item: result.value.toString(), route: 'defaultHome' }
+  seachList.value.push(searchResult)
+  if (seachList.value.length > 5) seachList.value.shift()
 }
 </script>
 
 <style lang="scss">
-  .search {
-    &__top {
-      display: flex;
-      align-items: center;
-      gap: rem(12px);
-      padding: rem(9px) rem(16px);
-      border-bottom: solid rem(1px) $neutral-lightgrey;
-      a {
-        &::before {
-          content: '';
-          display: block;
-          width: rem(24px);
-          height: rem(24px);
-          background-image: url('@/assets/images/icon/search_back.svg');
-        }
-      }
-    }
-    &__bottom {
-      padding: rem(24px) rem(16px);
-      overflow-y: scroll;
-      -ms-overflow-style: none;
-      scrollbar-width: none;
-      p {
-        font-size: rem(10px);
-        color: $neutral-basegrey;
-      }
-      ul {
-        margin-top: rem(20px);
-        li {
-          position: relative;
-          font-size: rem(14px);
-          font-weight: 400;
-          cursor: pointer;
-          a {
-            display: block;
-            width: 100%;
-          }
-          & + li {
-            margin-top: rem(24px);
-          }
-          &::before {
-            content: '';
-            position: absolute;
-            right: 0;
-            display: block;
-            width: rem(20px);
-            height: rem(20px);
-            background-image: url('@/assets/images/icon/search_recent.svg');
-            z-index: -1;
-          }
-        }
-      }
-      &::-webkit-scrollbar {
-        display: none;
+.search {
+  &__wrap {
+    height: 100vh;
+  }
+  &__top {
+    display: flex;
+    align-items: center;
+    gap: rem(12px);
+    padding: rem(9px) rem(16px);
+    border-bottom: solid rem(1px) $neutral-lightgrey;
+    a {
+      &::before {
+        content: '';
+        display: block;
+        width: rem(24px);
+        height: rem(24px);
+        background-image: url('@/assets/images/icon/search_back.svg');
       }
     }
   }
+  &__bottom {
+    height: calc(100vh - rem(55px));
+    padding: rem(24px) rem(16px);
+    overflow-y: scroll;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+    p {
+      font-size: rem(10px);
+      color: $neutral-basegrey;
+    }
+    ul {
+      margin-top: rem(20px);
+      li {
+        position: relative;
+        font-size: rem(14px);
+        font-weight: 400;
+        cursor: pointer;
+        a {
+          display: block;
+          width: 100%;
+        }
+        & + li {
+          margin-top: rem(24px);
+        }
+        &::before {
+          content: '';
+          position: absolute;
+          right: 0;
+          display: block;
+          width: rem(20px);
+          height: rem(20px);
+          background-image: url('@/assets/images/icon/search_recent.svg');
+          z-index: -1;
+        }
+      }
+    }
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+}
 </style>
